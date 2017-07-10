608af30fe030749f8e0ed373cba8011397fdf109
yunpeng.li <yunpeng.li@baesystems.com>
2017-07-10 12:10:33 -0400

    1. Use H2 database. 2. Build index in two ways.

--- src/main/java/com/library/essay/applications/MainApplication.java
+++ src/main/java/com/library/essay/applications/MainApplication.java
@@ -9,6 +9,10 @@ import org.apache.lucene.queryParser.MultiFieldQueryParser;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.util.Version;
+import org.hibernate.CacheMode;
+import org.hibernate.FlushMode;
+import org.hibernate.ScrollMode;
+import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.search.FullTextQuery;
@@ -18,128 +22,190 @@ import org.hibernate.search.Search;
 import com.library.essay.persistence.entities.Essay;
 import com.library.essay.utils.HibernateUtil;
 
+
 public class MainApplication {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		MainApplication application = new MainApplication();
+    MainApplication application = new MainApplication();
 
-		List<Serializable> idList = new ArrayList<Serializable>();
+    List<Serializable> idList = new ArrayList<Serializable>();
 
-		for (int i = 0; i < 10; i++) {
-			Serializable id = application.saveNewEssay("123-456-000" + i,
-					"Cliff Lee", "HHH CDF" + i);
+    for (int i = 0; i < 100; i++) {
+      Serializable id = application.saveNewEssay("123-456-000" + i, "Cliff Lee", "HHH CDF" + i);
 
-			idList.add(id);
-		}
+      idList.add(id);
+    }
 
-		application.printEssays(idList);
+    application.printEssays(idList);
 
-		application.search();
+    application.search();
 
-		// Clean up
-		application.deleteEssays(idList);
-	}
+    application.buildIndex();
 
-	public void search() {
+    application.buildIndexBatch();
 
-		Session session = HibernateUtil.getSessionFactory().openSession();
-		FullTextSession fullTextSession = Search.getFullTextSession(session);
+    // Clean up
+    application.deleteEssays(idList);
+    
+    System.exit(0);
+  }
 
-		String field0 = "title";
-		String field1 = "content";
+  public void buildIndex() {
 
-		String queryString0 = "456";
-		String queryString1 = "CDF8";
+    System.out.println("Start building index......");
 
-		String[] fields = new String[] { field0, field1 };
-		String[] queries = new String[] { queryString0, queryString1 };
+    Session session = HibernateUtil.getSessionFactory().openSession();
 
-		Transaction tx = fullTextSession.beginTransaction();
+    // Index the hazards
+    FullTextSession fullTextSession = Search.getFullTextSession(session);
+    fullTextSession.setFlushMode(FlushMode.MANUAL);
+    fullTextSession.setCacheMode(CacheMode.IGNORE);
+    Transaction transaction = fullTextSession.beginTransaction();
+    
+    // Scrollable results will avoid loading too many objects in memory
+    ScrollableResults results = fullTextSession.createCriteria(Essay.class).setFetchSize(100)
+        .scroll(ScrollMode.FORWARD_ONLY);
+    int index = 0;
+    while (results.next()) {
+      index++;
+      fullTextSession.index(results.get(0)); // index each element
+      if (index % 100 == 0) {
+        fullTextSession.flushToIndexes(); // apply changes to indexes
+        fullTextSession.clear(); // free memory since the queue is processed
+      }
+      System.out.println("in loop " + index);
+    }
+    transaction.commit();
 
-		try {
-			Query query = MultiFieldQueryParser.parse(Version.LUCENE_36,
-					queries, fields, new StandardAnalyzer(Version.LUCENE_36));
+    session.close();
 
-			System.out.println("=====================================");
-			System.out.println("Lucene query: " + query.toString());
-			System.out.println("=====================================");
+    System.out.println("End of building index.");
+  }
 
-			FullTextQuery hibQuery = fullTextSession.createFullTextQuery(query,
-					Essay.class);
+  public void buildIndexBatch() {
 
-			List<Essay> results = hibQuery.list();
+    System.out.println("Start building index......");
 
-			System.out.println("=====================================");
-			System.out.println("Found matched items:");
-			System.out.println("=====================================");
+    Session session = HibernateUtil.getSessionFactory().openSession();
 
-			for (Essay essay : results) {
-				System.out.println(essay);
-			}
+    // Index the hazards
+    FullTextSession fullTextSession = Search.getFullTextSession(session);
 
-		} catch (ParseException e) {
+    try {
 
-			e.printStackTrace();
-		}
+      fullTextSession.createIndexer().startAndWait();
 
-		tx.commit();
-		session.close();
-	}
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
 
-	public Essay getEssay(Serializable id) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
+    session.close();
 
-		Essay essay = (Essay) session.get(Essay.class, id);
+    System.out.println("End of building index.");
+  }
 
-		return essay;
-	}
+  public void search() {
 
-	public void printEssays(List<Serializable> idList) {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+    FullTextSession fullTextSession = Search.getFullTextSession(session);
 
-		System.out.println("=====================================");
-		System.out.println("Inserted Essays:");
-		System.out.println("=====================================");
+    String field0 = "title";
+    String field1 = "content";
 
-		for (Serializable id : idList) {
-			Essay essay = getEssay(id);
-			System.out.println(essay);
-		}
-	}
+    String queryString0 = "456";
+    String queryString1 = "CDF8";
 
-	public void deleteEssays(List<Serializable> idList) {
-		for (Serializable id : idList) {
-			deleteEssay(id);
-		}
-	}
+    String[] fields = new String[] {field0, field1};
+    String[] queries = new String[] {queryString0, queryString1};
 
-	public void deleteEssay(Serializable id) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
+    Transaction tx = fullTextSession.beginTransaction();
 
-		session.beginTransaction();
+    try {
+      Query query = MultiFieldQueryParser.parse(Version.LUCENE_36, queries, fields,
+          new StandardAnalyzer(Version.LUCENE_36));
 
-		Essay essay = (Essay) session.load(Essay.class, id);
-		session.delete(essay);
-		session.getTransaction().commit();
-		session.close();
-	}
+      System.out.println("=====================================");
+      System.out.println("Lucene query: " + query.toString());
+      System.out.println("=====================================");
 
-	public Serializable saveNewEssay(String title, String author, String content) {
-		Session session = HibernateUtil.getSessionFactory().openSession();
+      FullTextQuery hibQuery = fullTextSession.createFullTextQuery(query, Essay.class);
 
-		session.beginTransaction();
+      List<Essay> results = hibQuery.list();
 
-		Essay essay = new Essay();
-		essay.setTitle(title);
-		essay.setAuthor(author);
-		essay.setContent(content);
+      System.out.println("=====================================");
+      System.out.println("Found matched items:");
+      System.out.println("=====================================");
 
-		Serializable id = session.save(essay);
-		session.getTransaction().commit();
-		session.close();
+      for (Essay essay : results) {
+        System.out.println(essay);
+      }
 
-		return id;
+    } catch (ParseException e) {
 
-	}
+      e.printStackTrace();
+    }
+
+    tx.commit();
+    session.close();
+  }
+
+  public Essay getEssay(Serializable id) {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+
+    Essay essay = (Essay) session.get(Essay.class, id);
+
+    return essay;
+  }
+
+  public void printEssays(List<Serializable> idList) {
+
+    System.out.println("=====================================");
+    System.out.println("Inserted Essays:");
+    System.out.println("=====================================");
+
+    for (Serializable id : idList) {
+      Essay essay = getEssay(id);
+      System.out.println(essay);
+    }
+  }
+
+  public void deleteEssays(List<Serializable> idList) {
+    for (Serializable id : idList) {
+      deleteEssay(id);
+    }
+  }
+
+  public void deleteEssay(Serializable id) {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+
+    session.beginTransaction();
+
+    Essay essay = (Essay) session.load(Essay.class, id);
+    session.delete(essay);
+    session.getTransaction().commit();
+    session.close();
+
+    System.out.println("Essay " + id + " has been deleted.");
+  }
+
+  public Serializable saveNewEssay(String title, String author, String content) {
+    Session session = HibernateUtil.getSessionFactory().openSession();
+
+    session.beginTransaction();
+
+    Essay essay = new Essay();
+    essay.setTitle(title);
+    essay.setAuthor(author);
+    essay.setContent(content);
+
+    Serializable id = session.save(essay);
+    session.getTransaction().commit();
+    session.close();
+
+    return id;
+
+  }
 
 }
